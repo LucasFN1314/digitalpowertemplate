2426cc492161daac3980d217cde8c22c6a677b6d
Your Name <you@example.com>
2024-07-30 17:02:45 -0300

    Services

---
+++ src/utils/apiReq.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { show } from "./notification.js";
+
+export const backendUrl = "";
+
+const service = axios.create({
+    baseURL: `${backendUrl}/api`
+});
+
+let user = "";
+if (localStorage.getItem("dp_user") !== "undefined")
+    user = JSON.parse(localStorage.getItem("dp_user"));
+const config = {
+    headers: {
+        Authorization: `Bearer ${user?.token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+};
+
+export const post = (path, body) => {
+    return new Promise(async (resolve) => {
+        service
+            .post(path, { data: body }, config)
+            .then((response) => {
+                let message = response?.message ?? response.data?.message;
+                let status = response?.status ?? response.data?.status;
+
+                if (message) {
+                    show(message);
+                }
+                setTimeout(() => {
+                    if (response?.data?.redirect) {
+                        location.href = response?.data?.redirect;
+                    }
+                }, 1000);
+
+                if (status === 401) {
+                    location.href = "/admin";
+                }
+                resolve(response);
+            })
+            .catch((err) => {
+                console.log(err)
+                if (err.response.status === 401) {
+                    localStorage.removeItem("dp_user");
+                    location.href = "/login";
+                }
+                show("Ha ocurrido un error, intente mas tarde");
+            })
+            .finally(() => { });
+    });
+};
+export const files = (path, body) => {
+    return new Promise((resolve) => {
+        service
+            .post(path, body, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    Authorization: `Bearer ${user?.token}`,
+                    Accept: "application/json",
+                },
+            })
+            .then((response) => {
+                if (response.data?.message) {
+                    show(response.data.message);
+                }
+                setTimeout(() => {
+                    if (response?.data?.data?.redirect)
+                        location.href = response?.data?.data?.redirect;
+                }, 1000);
+
+                if (response.data?.status === 401) {
+                    location.href = "/admin";
+                }
+
+                resolve(response.data);
+            })
+            .catch((err) => {
+                if (err?.response?.status === 401) {
+                    location.href = "/login";
+                }
+                show("Ha ocurrido un error, intente mas tarde");
+            })
+            .finally(() => { });
+    });
+};
---
+++ src/utils/notification.js
@@ -0,0 +1,55 @@
+export const show = (text) => {
+    let html = `
+        ${text}
+    `;
+    let element = document.createElement("div");
+    element.innerHTML = html;
+    element.classList.add("notification");
+    element.classList.add("notification-enter");
+
+    document.getElementById("notification-container").appendChild(element);
+    setTimeout(() => {
+        element.classList.remove("notification-enter")
+        element.classList.add("notification-exit");
+    }, 3000);
+
+    setTimeout(() => {
+        element.remove();
+    }, 3240);
+}
+
+export const ConfirmModal = () => {
+    return new Promise((resolve) => {
+        let container = document.getElementById("app-layout");
+        let html = document.createElement("div");
+        html.setAttribute("id", "confirmModal");
+        html.innerHTML = `
+        <div class="overlay" overlay>
+        </div>
+        <div class="custom-modal-html" shadow fade radius-border>
+            <div class="title">
+                <h1>Alerta</h1>
+            </div>
+            <div class="content">
+                <p>¿Esta seguro que desea continuar con esta accion? Es posible que no tenga retroceso</p>
+                <br>
+                <div class="actions" flex-center>
+                    <a class="custom-button-html red" id="confirmModalButton">Continuar</a>
+                    <a class="custom-button-html" id="cancelModalButton">Cancelar</a>
+                </div>
+            </div>
+        </div>
+        `;
+        container.appendChild(html);
+
+        document.getElementById("confirmModalButton").addEventListener("click", (e) => {
+            html.remove();
+            resolve(true);
+        })
+
+        document.getElementById("cancelModalButton").addEventListener("click", (e) => {
+            html.remove();
+            resolve(false);
+        })
+    })
+}
