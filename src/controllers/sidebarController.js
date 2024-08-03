7663827dfb8b75a665cc5df4eeebb34dd226420d
Your Name <you@example.com>
2024-08-02 23:43:31 -0300

    Fix

--- src/controllers/sidebarController.js
+++ src/controllers/sidebarController.js
@@ -13,14 +13,30 @@ const adminFields = [
         label: 'Agregar Escuelas',
         url: "/escuelas",
         icon: "buildings",
-    }
+    },
+    {
+        id: 1,
+        label: 'Agregar Cursos',
+        url: "/cursos",
+        icon: "building",
+    },
+    {
+        id: 1,
+        label: 'Agregar Alumnos',
+        url: "/alumnos",
+        icon: "user",
+    },
+    {
+        id: 1,
+        label: 'Agregar Examen',
+        url: "/examenes",
+        icon: "spreadsheet",
+    },
 ];
 export default function (user) {
     if (!user) return;
-    if (user?.admin) {
-        fields = [...fields, ...adminFields];
-    }
-    else {
+
+    if (user?.admin === 1) {
         fields = [...fields, ...adminFields];
     }
     return fields;
